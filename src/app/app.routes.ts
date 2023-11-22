07df3939f2ffe9b67dc3ea770bc565518626de23
Jramos <johnatan.r1000@gmail.com>
2023-11-22 16:33:25 -0500

    instalacion de tailwind.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListHotelComponent } from './domains/hotel/list-hotel/list-hotel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: ListHotelComponent
+}];
